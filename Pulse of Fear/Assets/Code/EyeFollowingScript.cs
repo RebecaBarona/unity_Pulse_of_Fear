a1d4a911033acbf8d6d13bfdb24930ed8ef29af4
RebecaBarona <beky_barona1@hotmail.com>
2024-02-12 00:03:00 +0100

    Updated textures

--- Pulse of Fear/Assets/Code/EyeFollowingScript.cs
+++ Pulse of Fear/Assets/Code/EyeFollowingScript.cs
@@ -8,18 +8,14 @@ public class EyeFollowingPlayer : MonoBehaviour
     public bool moveAlongX = false;
     public bool moveAlongY = false;
     public bool moveAlongZ = false;
-    public float detectionRadius = 5;
-    public float speed = 1f;
 
     private Vector3 startPosition;
-    private Vector3 playerPreviousPosition;
     private Vector3 size;
 
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        playerPreviousPosition = player.transform.position;
+        player = GameObject.FindGameObjectWithTag("FakePlayer");
         startPosition = transform.position;
         MeshRenderer renderer = GetComponent<MeshRenderer>();
         size = renderer.bounds.size;
@@ -29,49 +25,29 @@ public class EyeFollowingPlayer : MonoBehaviour
 
     void Update()
     {
-        Vector3 playerDelta = player.transform.position - playerPreviousPosition;
-        move(playerDelta);
-        playerPreviousPosition = player.transform.position;
-    }
-
-
-    void move(Vector3 playerDelta)
-    {
-        playerDelta *= speed;
-        Vector3 newPos = transform.position + playerDelta;
-        float distanceFromStartingPosition = Vector3.Distance(newPos, startPosition);
-        float distancePlayerEye = Vector3.Distance(player.transform.position, transform.position);
+        Vector3 localStartPos = transform.InverseTransformPoint(startPosition);
+        Vector3 delta = transform.InverseTransformDirection(player.transform.position - transform.position);
         float maxSize = Mathf.Max(size.x, size.y, size.z);
-        if (distanceFromStartingPosition > maxSize)
+        Vector3 translationVector = Vector3.zero;
+        if (moveAlongX)
         {
-            return;
+            translationVector = new Vector3(delta.x, 0, 0);
+
         }
-        if (distancePlayerEye > detectionRadius)
+        else if (moveAlongZ)
         {
-            return;
+            translationVector = new Vector3(0, 0, delta.z);
+
         }
-        float maxX = startPosition.x + maxSize;
-        float minX = startPosition.x - maxSize;
-
-        float maxY = startPosition.y + maxSize;
-        float minY = startPosition.y - maxSize;
-
-        float maxZ = startPosition.z + maxSize;
-        float minZ = startPosition.z - maxSize;
-
-        if (moveAlongX && player.transform.position.x > minX && player.transform.position.x < maxX)
+        else if (moveAlongY)
         {
-            transform.Translate(new Vector3(playerDelta.x, 0, 0));
+            translationVector = new Vector3(0, delta.y, 0);
         }
-
-        if (moveAlongY && player.transform.position.y > minY && player.transform.position.y < maxY)
+        Vector3 newPosLocal = transform.InverseTransformPoint(transform.position) + translationVector;
+        float distanceLocal = Vector3.Distance(newPosLocal, localStartPos);
+        if (distanceLocal < maxSize)
         {
-            transform.Translate(new Vector3(0, playerDelta.y, 0));
-        }
-
-        if (moveAlongZ && player.transform.position.z > minZ && player.transform.position.z < maxZ)
-        {
-            transform.Translate(new Vector3(0, 0, playerDelta.z));
+            transform.Translate(translationVector);
         }
 
     }
