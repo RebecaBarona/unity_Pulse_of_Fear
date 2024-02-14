830696c5e12be3c77820462f08cd7a0abdec2c51
RebecaBarona <beky_barona1@hotmail.com>
2024-02-14 20:59:16 +0100

    Eyes placed

--- Pulse of Fear/Assets/Code/EyeFollowingScript.cs
+++ Pulse of Fear/Assets/Code/EyeFollowingScript.cs
@@ -1,28 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EyeFollowingPlayer : MonoBehaviour
 {
     private GameObject player;
     private Vector3 eyeStartPosition;
-    private Vector3 previousPlayerPos;
     private float size;
     public float detectionRange = 3;
     public float speed = 0.8f;
-    public float maxDistance = 1;
-    private bool isMoving = false;
+    public float maxDistance = 10;
 
 
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("MainCamera");
         MeshRenderer renderer = GetComponent<MeshRenderer>();
-        Vector3 size = renderer.bounds.size * maxDistance;
+        Vector3 size = renderer.bounds.size;
         eyeStartPosition = transform.position;
-        previousPlayerPos = toLocal(player.transform.position);
-        this.size = Mathf.Max(size.x, size.y, size.z);
+        this.size = Mathf.Max(size.x, size.y, size.z) * maxDistance;
     }
 
     Vector3 toLocal(Vector3 vector)
@@ -36,38 +31,25 @@ public class EyeFollowingPlayer : MonoBehaviour
     {
         Vector3 currentPlayerPosition = toLocal(player.transform.position);
         Vector3 currentEyePosition = toLocal(transform.position);
-        float distancePlayerEyeX = Mathf.Abs((currentEyePosition - currentPlayerPosition).x);
 
-        if (!isMoving)
+
+
+        Vector3 newEyePositionLocal = new Vector3(Mathf.Clamp(currentPlayerPosition.x, -size, size), currentEyePosition.y,
+            currentEyePosition.z);
+        Vector3 newEyePositionGlobal = transform.TransformPoint(newEyePositionLocal);
+        float distanceEyeToStartPosition = Vector3.Distance(eyeStartPosition, newEyePositionGlobal);
+        bool inDetectionRange = Vector3.Distance(player.transform.position, transform.position) < detectionRange;
+
+        if (distanceEyeToStartPosition < size && inDetectionRange)
         {
-            isMoving = previousPlayerPos.x >= currentEyePosition.x && currentPlayerPosition.x <= currentEyePosition.x || previousPlayerPos.x <= currentEyePosition.x && currentPlayerPosition.x >= currentEyePosition.x;
+            transform.position = newEyePositionGlobal;
         }
 
-        if (isMoving)
+
+        if (!inDetectionRange)
         {
-            Vector3 newEyePositionLocal = new Vector3(Mathf.Clamp(currentPlayerPosition.x, -size, size), currentEyePosition.y, currentEyePosition.z);
-            Vector3 newEyePositionGlobal = transform.TransformPoint(newEyePositionLocal);
-            float distanceEyeToStartPosition = Vector3.Distance(eyeStartPosition, newEyePositionGlobal);
-            bool inDetectionRange = Vector3.Distance(player.transform.position, transform.position) < detectionRange;
-
-            if (distanceEyeToStartPosition < size && inDetectionRange)
-            {
-                transform.position = newEyePositionGlobal;
-            }
-
-
-            if (!inDetectionRange)
-            {
-                float step = speed * Time.deltaTime; // Calculate distance to move
-                transform.position = Vector3.MoveTowards(transform.position, eyeStartPosition, step);
-                if (transform.position == eyeStartPosition)
-                {
-                    isMoving = false;
-                }
-
-            }
+            float step = speed * Time.deltaTime; // Calculate distance to move
+            transform.position = Vector3.MoveTowards(transform.position, eyeStartPosition, step);
         }
-
-        previousPlayerPos = currentPlayerPosition;
     }
 }
