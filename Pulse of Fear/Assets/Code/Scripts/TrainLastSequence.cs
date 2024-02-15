bbd1a8407e2e3fafb47c86d19949ca0a727687c2
gamzesunar <78363022+gamzesunar@users.noreply.github.com>
2024-02-15 21:26:01 +0100

    train last sequence

--- Pulse of Fear/Assets/Code/Scripts/TrainLastSequence.cs
+++ Pulse of Fear/Assets/Code/Scripts/TrainLastSequence.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class AnimationTrigger : MonoBehaviour
+public class TrainLastSequence : MonoBehaviour
 {
     public Animator animator; // Reference to the Animator component
-    public string animationName = "Level02BuildingOpen"; // Name of the animation to play
+    public string animationName = "TrainLightAnimation"; // Name of the animation to play
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) // Check if the collider is tagged as "Player"
         {
-            //  animator = GetComponent<Animator>(); // Get the Animator component attached to this GameObject
-            animator.SetBool("wall", true); // Play the specified animation
+          
+            animator.SetBool(animationName, true); // Play the specified animation
         }
     }
 }
